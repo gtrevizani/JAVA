3bb277a1ee30a6936b8e193f901096818b5bc562
shilm04 <guilhermetrevz@outlook.com>
2022-04-07 17:02:03 -0300

    v123

---
+++ src/exercicios_sala_de_aula/src/iteracao/iteracaoWhile.java
@@ -0,0 +1,37 @@
+package iteracao;
+
+public class iteracaoWhile {
+    public static void main(String[] args) {
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("Eu amo Java!");
+        }
+
+        int i = 0;
+        while (i<10){
+            System.out.println("Eu amo Java!");
+            i++
+        }
+
+        int i = 0;
+        while (i<10){
+            if(i==5){
+                System.out.println("Vai parar!");
+                break;
+            }
+            System.out.println(i + ": Eu amo Java");
+            i++;
+        }
+
+        int i =0;
+        while (i<10){
+            if(i==5){
+                System.out.println("Vai continuar...");
+                i++;
+                continue
+            }
+            System.out.println("Eu amo IntelliJ");
+            i++
+        }
+         */
+    }
+}
