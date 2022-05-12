6b2fac634877258269619fbb8d32c40fbeac6d0c
shilm04 <guilhermetrevz@outlook.com>
2022-05-12 17:11:13 -0300

    v123

---
+++ src/orientacao_a_objeto/src/orientacao_a_objetos_parte2/o_objeto_this/desafio_objeto_this_e_construtores/Fornecedor.java
@@ -0,0 +1,13 @@
+package orientacao_a_objetos_parte2.o_objeto_this.desafio_objeto_this_e_construtores;
+
+public class Fornecedor {
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
