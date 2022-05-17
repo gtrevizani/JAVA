62183d060ea8bb42131790de145a0c0285c61947
shilm04 <guilhermetrevz@outlook.com>
2022-05-17 17:15:58 -0300

    v113

---
+++ src/orientacao_a_objeto/src/orientacao_a_objetos_parte2/desafio_pacotes_e_enumeracoes_5_13/ContaPagar.java
@@ -0,0 +1,82 @@
+package orientacao_a_objetos_parte2.desafio_pacotes_e_enumeracoes_5_13;
+
+public class ContaPagar { // Primeiro.
+    String descricao;
+    Double valor;
+    String dataVencimento;
+    Fornecedor fornecedor;
+    private SituacaoConta situacaoConta;
+    public ContaPagar() {
+        this.situacaoConta = SituacaoConta.PENDENTE;
+    }
+    public ContaPagar(String descricao, Double valor, String dataVencimento, Fornecedor fornecedor) { // Constructor
+        this();
+        this.descricao = descricao;
+        this.valor = valor;
+        this.dataVencimento = dataVencimento;
+        this.fornecedor = fornecedor;
+    }
+    void pagar(){
+        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())){
+            System.out.println("Não pode pagar uma conta que já está paga: "
+                    + this.getDescricao() + ".");
+        } else if (SituacaoConta.CANCELADA.equals(this.getSituacaoConta())) {
+            System.out.println("Não pode pagar uma conta que está cancelada: "
+                    + this.getDescricao() + ".");
+        } else {
+            System.out.println("Pagando conta " + this.getDescricao() +
+                    " no valor de " + this.getValor() +
+                    " e vencimento em " + this.getDataVencimento() +
+                    " do fornecedor " + this.getFornecedor().getNome() + ".");
+            this.situacaoConta = SituacaoConta.PAGA;
+        }
+    }
+    public void cancelar() {
+        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())){
+            System.out.println("Não pode cancelar uma conta que foi paga: "
+                    + this.getDescricao() + ".");
+        } else if (SituacaoConta.CANCELADA.equals(this.getSituacaoConta())) {
+            System.out.println("Não pode cancelar uma conta que já foi cancelada: "
+                    + this.getDescricao() + ".");
+        } else {
+            System.out.println("Cancelando conta " + this.getDescricao() + ".");
+            // altera situação da conta para CANCELADA
+            this.situacaoConta = SituacaoConta.CANCELADA;
+        }
+    }
+    public String getDescricao() { // Getter and Setters.
+        return descricao;
+    } // Getter and Setters;
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public SituacaoConta getSituacaoConta(){
+        return this.situacaoConta;
+    }
+}
---
+++ src/orientacao_a_objeto/src/orientacao_a_objetos_parte2/desafio_pacotes_e_enumeracoes_5_13/Fornecedor.java
@@ -0,0 +1,13 @@
+package orientacao_a_objetos_parte2.desafio_pacotes_e_enumeracoes_5_13;
+
+public class Fornecedor {
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
