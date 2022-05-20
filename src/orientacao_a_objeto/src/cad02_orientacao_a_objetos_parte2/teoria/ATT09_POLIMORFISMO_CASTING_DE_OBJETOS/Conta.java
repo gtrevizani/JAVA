e2bf7f77e3227b078738587fe3896a421ef2d70a
shilm04 <guilhermetrevz@outlook.com>
2022-05-20 17:02:36 -0300

    v123

---
+++ src/orientacao_a_objeto/src/cad02_orientacao_a_objetos_parte2/teoria/ATT09_POLIMORFISMO_CASTING_DE_OBJETOS/Conta.java
@@ -0,0 +1,12 @@
+package cad02_orientacao_a_objetos_parte2.teoria.ATT09_POLIMORFISMO_CASTING_DE_OBJETOS;
+
+public class Conta {
+    protected double saldo;
+
+    public double getSaldo(){
+        return saldo;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+}
---
+++ src/orientacao_a_objeto/src/cad02_orientacao_a_objetos_parte2/teoria/ATT09_POLIMORFISMO_CASTING_DE_OBJETOS/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package cad02_orientacao_a_objetos_parte2.teoria.ATT09_POLIMORFISMO_CASTING_DE_OBJETOS;
+
+public class ContaPoupanca extends Conta{
+    private double rendimentos = 30;
+
+    public double getRendimentos() {
+        return rendimentos;
+    }
+}
