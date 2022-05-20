b3a0c4c436dc794f1a170c149339dd66e56d96ed
shilm04 <guilhermetrevz@outlook.com>
2022-05-20 14:26:46 -0300

    v123

---
+++ src/orientacao_a_objeto/src/orientacao_a_objetos_parte2/pratica/desafio_heranca_e_sobreposicao/Cliente.java
@@ -0,0 +1,5 @@
+package orientacao_a_objetos_parte2.pratica.desafio_heranca_e_sobreposicao;
+
+public class Cliente extends Pessoa {
+
+}
---
+++ src/orientacao_a_objeto/src/orientacao_a_objetos_parte2/pratica/desafio_heranca_e_sobreposicao/SituacaoConta.java
@@ -0,0 +1,9 @@
+package orientacao_a_objetos_parte2.pratica.desafio_heranca_e_sobreposicao;
+
+public enum SituacaoConta {
+
+    PENDENTE,
+    PAGA,
+    CANCELADA
+
+}
