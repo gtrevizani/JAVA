813cb3981778031b0179205385c39439e291dcd1
shilm04 <99826402+shilm04@users.noreply.github.com>
2022-05-27 17:59:14 -0300

    v123

--- src/_03_orientacao_a_objeto/src/cad02_orientacao_a_objetos_parte2/teoria/ATT07_SOBREPOSICAO/ProdutoPerecivel.java
+++ src/_03_orientacao_a_objeto/src/cad02_orientacao_a_objetos_parte2/teoria/ATT07_SOBREPOSICAO/ProdutoPerecivel.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class ProdutoPerecivel extends Produto{
     protected Date dataValidade; // Precisa instanciar
     public void identificar(){
-        super.identificar(); // super = invoca o método.
+        super.identificar(); // super = invoca o método diretamente do objeto.
         System.out.println("Minha data de validade é: " + dataValidade);
     }
 }
