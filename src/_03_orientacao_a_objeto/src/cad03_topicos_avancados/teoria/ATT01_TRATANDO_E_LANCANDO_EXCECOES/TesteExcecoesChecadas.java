a7294286b9b5fc46f11cb6979169aff5c0b577af
Guilherme I. Trevizani <99826402+shilm04@users.noreply.github.com>
2022-05-31 15:58:51 -0300

    Update TesteExcecoesChecadas.java

--- src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/TesteExcecoesChecadas.java
+++ src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/TesteExcecoesChecadas.java
@@ -1,4 +1,25 @@
 package cad03_topicos_avancados.teoria.ATT01_TRATANDO_E_LANCANDO_EXCECOES;
 
 public class TesteExcecoesChecadas {
+
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(100);
+
+        try {
+            cc.sacar(60);
+        } catch ( SaldoInsuficienteException e) {
+            System.out.printf("Desculpe, saldo insuficiente.");
+        }
+        System.out.println("Saldo: " + cc.getSaldo());
+
+        try {
+            cc.sacar(50);
+        } catch ( SaldoInsuficienteException e) {
+            System.out.println("Desculpe, saldo insuficiente.");
+        }
+        finally {
+            System.out.println("Obrigado por utilizar nosso sitema!");
+        }
+        System.out.println("Saldo agora: " + cc.getSaldo());
+    }
 }
