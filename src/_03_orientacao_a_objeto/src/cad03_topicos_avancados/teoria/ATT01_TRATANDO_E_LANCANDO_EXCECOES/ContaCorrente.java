fadac3f4a8a602058272d400e5e05e0f51ff08ab
Guilherme I. Trevizani <99826402+shilm04@users.noreply.github.com>
2022-05-31 15:57:30 -0300

    Update ContaCorrente.java

--- src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/ContaCorrente.java
+++ src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/ContaCorrente.java
@@ -1,4 +1,30 @@
 package cad03_topicos_avancados.teoria.ATT01_TRATANDO_E_LANCANDO_EXCECOES;
 
 public class ContaCorrente {
+
+    private double saldo;
+
+    public ContaCorrente(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double deposito) {
+        if ( deposito <= 0) {
+            throw new IllegalArgumentException("O valor não pode ser menor que zero.");
+        }
+        this.saldo += deposito;
+    }
+
+    public void sacar(double quantidade) throws SaldoInsuficienteException {
+        double saldoTemp = saldo - quantidade;
+        if (saldoTemp < 0) {
+            throw  new SaldoInsuficienteException("Você não possui saldo suficiente");
+        }
+        this.saldo -= quantidade;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
 }
