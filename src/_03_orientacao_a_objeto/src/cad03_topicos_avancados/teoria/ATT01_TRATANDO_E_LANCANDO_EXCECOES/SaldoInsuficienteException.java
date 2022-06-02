b8587ea3c339ea725e84e3200a469d21f78505dd
Guilherme I. Trevizani <99826402+shilm04@users.noreply.github.com>
2022-06-02 14:46:15 -0300

    Update SaldoInsuficienteException.java

--- src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/SaldoInsuficienteException.java
+++ src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/SaldoInsuficienteException.java
@@ -4,5 +4,5 @@ public class SaldoInsuficienteException extends Exception {
 
     public SaldoInsuficienteException(String mensagem) {
         super(mensagem);
-    }
+    } // Método das exceções.
 }
