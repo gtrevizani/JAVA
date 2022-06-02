4a906c40fe0d19716f9fd017774952e7ecff69e3
Guilherme I. Trevizani <99826402+shilm04@users.noreply.github.com>
2022-06-02 14:48:52 -0300

    Delete SaldoInsuficienteException.java

--- src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/pratica/_06_01_desafio_excecoes/SaldoInsuficienteException.java
+++
@@ -1,8 +0,0 @@
-package cad03_topicos_avancados.teoria.ATT01_TRATANDO_E_LANCANDO_EXCECOES;
-
-public class SaldoInsuficienteException extends Exception {
-
-    public SaldoInsuficienteException(String mensagem) {
-        super(mensagem);
-    } // Método das exceções.
-}
