71cc040190f5bb7a7a1393650d66d2419369f652
Guilherme I. Trevizani <99826402+shilm04@users.noreply.github.com>
2022-06-02 14:46:49 -0300

    Update TesteExcecoesChecadas.java

--- src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/TesteExcecoesChecadas.java
+++ src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT01_TRATANDO_E_LANCANDO_EXCECOES/TesteExcecoesChecadas.java
@@ -3,21 +3,21 @@ package cad03_topicos_avancados.teoria.ATT01_TRATANDO_E_LANCANDO_EXCECOES;
 public class TesteExcecoesChecadas {
 
     public static void main(String[] args) {
-        ContaCorrente cc = new ContaCorrente(100);
+        ContaCorrente cc = new ContaCorrente(110); // Constructor
 
-        try {
-            cc.sacar(60);
-        } catch ( SaldoInsuficienteException e) {
-            System.out.printf("Desculpe, saldo insuficiente.");
+        try { // Teste
+            cc.sacar(500);
+        } catch ( SaldoInsuficienteException e) { // Try não é o correto, execute o Catch.
+            System.out.printf("Desculpe, saldo insuficiente.\n");
         }
         System.out.println("Saldo: " + cc.getSaldo());
 
-        try {
+        try { // Teste
             cc.sacar(50);
-        } catch ( SaldoInsuficienteException e) {
+        } catch ( SaldoInsuficienteException e) { // Try não é o correto, execute o Catch.
             System.out.println("Desculpe, saldo insuficiente.");
         }
-        finally {
+        finally { // Por fim, execute o Finally.
             System.out.println("Obrigado por utilizar nosso sitema!");
         }
         System.out.println("Saldo agora: " + cc.getSaldo());
