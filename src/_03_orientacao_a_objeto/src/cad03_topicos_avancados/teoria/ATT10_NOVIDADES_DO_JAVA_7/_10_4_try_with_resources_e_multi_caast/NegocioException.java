1c8a1e09ffd9d2d4e50c270d11c81383d397af20
shilm04 <guilhermetrevz@outlook.com>
2022-06-21 17:01:14 -0300

    v123

--- src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT10_NOVIDADES_DO_JAVA_7/_10_4_try_with_resources_e_multi_caast/NegocioException.java
+++ src/_03_orientacao_a_objeto/src/cad03_topicos_avancados/teoria/ATT10_NOVIDADES_DO_JAVA_7/_10_4_try_with_resources_e_multi_caast/NegocioException.java
@@ -1,4 +1,4 @@
-package cad03_topicos_avancados.teoria.ATT10_NOVIDADES_DO_JAVA_7._10_4_try_with_resources_e_multi_catch;
+package cad03_topicos_avancados.teoria.ATT10_NOVIDADES_DO_JAVA_7._10_4_try_with_resources_e_multi_caast;
 
 public class NegocioException extends RuntimeException{
     public NegocioException(String msg){
